ab9b075846a640059d4aa3511d81ef65725a603b
Jean-Baptiste Queru <jbqueru@gmail.com>
2024-08-06 09:54:50 +0300

    Display the vertical scrollers as a wave

--- mb24_vscroll-st_rmac.s
+++ mb24_vscroll-st_rmac.s
@@ -42,9 +42,12 @@ VertInit:
 	move.l	#VertFont + 2, vert_char_end
 	move.l	#VertText, vert_text_read
 
-	move.l	#VerticalCurve, vert_curve1
-	move.l	#VerticalCurve + 20, vert_curve2
-	move.l	#VerticalCurve + 40, vert_curve3
+	move.l	#VerticalCurve + 75, vert_curve1
+	move.l	#VerticalCurve + 60, vert_curve2
+	move.l	#VerticalCurve + 45, vert_curve3
+	move.l	#VerticalCurve + 30, vert_curve4
+	move.l	#VerticalCurve + 15, vert_curve5
+	move.l	#VerticalCurve + 00, vert_curve6
 
 	rts
 
@@ -63,55 +66,86 @@ VertInit:
 ; *************************
 VertDraw:
 
-	move.l	fb_back, a0
 	move.l	vert_buffer_read, a1
 	movea.l	a1, a2
 	movea.l	a1, a3
+	movea.l	a1, a4
+	movea.l	a1, a5
+	movea.l	a1, a6
 
 	moveq.l	#0, d0
 
-	movea.l	vert_curve1, a4
-	move.b	(a4)+, d0
-	cmpa.l	#VerticalCurveEnd, a4
+	movea.l	vert_curve1, a0
+	move.b	(a0)+, d0
+	cmpa.l	#VerticalCurveEnd, a0
 	bne.s	.InCurve1
-	lea.l	VerticalCurve, a4
+	lea.l	VerticalCurve, a0
 .InCurve1:
-	move.l	a4, vert_curve1
+	move.l	a0, vert_curve1
 	adda.w	d0, a1
 
-	movea.l	vert_curve2, a4
-	move.b	(a4)+, d0
-	addi.b	#46, d0
-	cmpa.l	#VerticalCurveEnd, a4
+	movea.l	vert_curve2, a0
+	move.b	(a0)+, d0
+	;addi.b	#46, d0
+	cmpa.l	#VerticalCurveEnd, a0
 	bne.s	.InCurve2
-	lea.l	VerticalCurve, a4
+	lea.l	VerticalCurve, a0
 .InCurve2:
-	move.l	a4, vert_curve2
+	move.l	a0, vert_curve2
 	adda.w	d0, a2
 
-	movea.l	vert_curve3, a4
-	move.b	(a4)+, d0
-	addi.b	#92, d0
-	cmpa.l	#VerticalCurveEnd, a4
+	movea.l	vert_curve3, a0
+	move.b	(a0)+, d0
+	;addi.b	#92, d0
+	cmpa.l	#VerticalCurveEnd, a0
 	bne.s	.InCurve3
-	lea.l	VerticalCurve, a4
+	lea.l	VerticalCurve, a0
 .InCurve3:
-	move.l	a4, vert_curve3
+	move.l	a0, vert_curve3
 	adda.w	d0, a3
 
+	movea.l	vert_curve4, a0
+	move.b	(a0)+, d0
+	;addi.b	#92, d0
+	cmpa.l	#VerticalCurveEnd, a0
+	bne.s	.InCurve4
+	lea.l	VerticalCurve, a0
+.InCurve4:
+	move.l	a0, vert_curve4
+	adda.w	d0, a4
+
+	movea.l	vert_curve5, a0
+	move.b	(a0)+, d0
+	;addi.b	#92, d0
+	cmpa.l	#VerticalCurveEnd, a0
+	bne.s	.InCurve5
+	lea.l	VerticalCurve, a0
+.InCurve5:
+	move.l	a0, vert_curve5
+	adda.w	d0, a5
+
+	movea.l	vert_curve6, a0
+	move.b	(a0)+, d0
+	;addi.b	#92, d0
+	cmpa.l	#VerticalCurveEnd, a0
+	bne.s	.InCurve6
+	lea.l	VerticalCurve, a0
+.InCurve6:
+	move.l	a0, vert_curve6
+	adda.w	d0, a6
+
+	movea.l	fb_back, a0
+	lea.l	16(a0), a0
 	moveq.l	#19,d1
 .Draw10Lines:
 	.rept	10
-	move.w	(a1)+, d0
-	move.w	d0, 8(a0)
-	move.w	d0, 144(a0)
-	move.w	(a2)+, d0
-	move.w	d0, 24(a0)
-	move.w	d0, 128(a0)
-	move.w	(a3)+, d0
-	move.w	d0, 40(a0)
-	move.w	d0, 112(a0)
-	add.w	#160, a0
+	move.w	(a1)+, (a0)
+	move.w	(a2)+, 24(a0)
+	move.w	(a3)+, 48(a0)
+	move.w	(a4)+, 72(a0)
+	move.w	(a5)+, 96(a0)
+	move.w	(a6)+, 120(a0)
+	lea.l	160(a0), a0
 	.endr
 	dbra	d1, .Draw10Lines
 
@@ -145,7 +179,7 @@ VertDraw:
 .InChar:
 	move.l	a1, vert_char_read
 	move.w	d0, (a0)
-	move.w	d0, 652(a0)
+	move.w	d0, 654(a0)
 
 ; ***************************
 ; **                       **
@@ -155,7 +189,7 @@ VertDraw:
 
 	move.l	vert_buffer_read, a0
 	addq.w	#2, a0
-	cmp.l	#vert_buffer + 652, a0
+	cmp.l	#vert_buffer + 654, a0
 	bne.s	.BufferOk
 	move.l	#vert_buffer, a0
 .BufferOk:
@@ -166,7 +200,7 @@ VertDraw:
 	.data
 
 VertText:
-	dc.b	'!"#$ ! !   !!! !!! !!!   ! ! !      '
+	dc.b	' !"#$ ! !   !!! !!! !!!   ! ! !      '
 VertTextEnd:
 
 	.bss
@@ -181,6 +215,12 @@ vert_curve2:
 	ds.l	1
 vert_curve3:
 	ds.l	1
+vert_curve4:
+	ds.l	1
+vert_curve5:
+	ds.l	1
+vert_curve6:
+	ds.l	1
 
 vert_char_read:
 	ds.l	1
@@ -191,7 +231,7 @@ vert_text_read:
 	ds.l	1
 
 vert_buffer:
-	ds.w	2 * 326
+	ds.w	2 * 327
 
 	.include "mb24_vfont-st_rmac.s"
 	.include "tmp/mb24_vcurves-st_rmac.s"
